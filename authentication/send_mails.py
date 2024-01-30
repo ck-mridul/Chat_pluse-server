3d5eaaf05fd242038ffbcec8ceb7a83a46692171
ckmridul <ckmkanad16@gmail.com>
2024-01-30 09:46:01 +0530

    add static and chage the verification link

--- authentication/send_mails.py
+++ authentication/send_mails.py
@@ -5,5 +5,5 @@ from django.conf import settings
 @shared_task
 def send_verification_email(email,uid):
     verification_subject = 'Verify Your Account'
-    verification_message = 'Please click the link below to verify your account: http://127.0.0.1:3000/verify/{}/'.format(uid)
+    verification_message = 'Please click the link below to verify your account: https://chat-pulse.vercel.app/verify/{}/'.format(uid)
     send_mail(verification_subject, verification_message, settings.EMAIL_HOST_USER, [email])
